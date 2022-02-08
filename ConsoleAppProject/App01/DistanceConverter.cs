21fbb604804db02963598a0742ac3b13e3d2bc93
sambaker149 <91675987+sambaker149@users.noreply.github.com>
2022-02-08 16:08:40 +0000

    Added first methods with comments

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -1,13 +1,76 @@
-﻿namespace ConsoleAppProject.App01
+﻿using System;
+
+namespace ConsoleAppProject.App01
 {
     /// <summary>
-    /// Please describe the main features of this App
+    /// Allows the user to convert a distance in miles to feet
     /// </summary>
     /// <author>
-    /// Student Name version 0.1
+    /// Samuel Baker 08/02/2022
     /// </author>
     public class DistanceConverter
     {
+        public const int FEET_IN_MILES = 5280;
 
+        private double miles;
+        private double feet;
+        private string number;
+
+        /// <summary>
+        /// Output a heading
+        /// Ask for input for miles 
+        /// Calculate and output the same distance in feet.
+        /// </summary>
+        public void Run()
+        {
+            OutputHeading();
+
+            InputMiles();
+            CalculateFeet();
+            OutputFeet();
+        }
+
+        /// <summary>
+        /// Print the heading of the distance converter application
+        /// </summary>
+        private static void OutputHeading()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+
+            Console.WriteLine();
+            Console.WriteLine(" =============================== ");
+            Console.WriteLine("       Distance Converter        ");
+            Console.WriteLine("        by Samuel Baker          ");
+            Console.WriteLine(" =============================== ");
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Prompt to user to input the distance in miles
+        /// Input the miles as a double number
+        /// </summary>
+        private void InputMiles()
+        {
+            Console.Write(" Please input the distance in miles to be converted > ");
+
+            number = Console.ReadLine();
+            miles = Convert.ToDouble(number);
+        }
+
+        /// <summary>
+        /// Calculate the feet using the feet in miles conversion
+        /// </summary>
+        private void CalculateFeet()
+        {
+            feet = miles * FEET_IN_MILES;
+        }
+
+        /// <summary>
+        /// Print out the result of the conversion
+        /// </summary>
+        private void OutputFeet()
+        {
+            Console.WriteLine("The number of miles = " + feet);
+        }
     }
 }
