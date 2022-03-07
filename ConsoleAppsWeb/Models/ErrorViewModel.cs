fe861984b2aabdcd87d4930c47a216327c5764a4
sambaker149 <91675987+sambaker149@users.noreply.github.com>
2022-03-07 10:08:07 +0000

    Removed excess folders

--- ConsoleAppsWeb/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ConsoleAppsWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
